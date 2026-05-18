108d20c377d4c994740dba312d4ca6b8ea788fce
Guilherme Bueno Martins <gui.bue@uninove.edu.br>
2026-05-17 21:57:06 -0300

    Initial projet

---
+++ src/main/java/io/github/guilhermebuenomartins/smarttaskscheduler/SmartTaskSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.guilhermebuenomartins.smarttaskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskSchedulerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTaskSchedulerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/guilhermebuenomartins/smarttaskscheduler/SmartTaskSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guilhermebuenomartins.smarttaskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskSchedulerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
